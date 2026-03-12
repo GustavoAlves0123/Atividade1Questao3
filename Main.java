55debd971ec925bd45d2a09cae98d04e5db35b6c
Gustavo Alves <99749706+GustavoAlves0123@users.noreply.github.com>
2026-03-11 21:50:57 -0300

    Refactor Faculdade, Aluno, and Curso classes

--- Main.java
+++ Main.java
@@ -1,112 +1,128 @@
-class Faculdade {
-    String nome;
-    String endereco;
-    int quantidadeDeAlunos;
-    int quantidadeDeCursos;
-    String reitor;
+import java.util.ArrayList;
 
-    void adicionaAluno(Aluno aluno) {
-        quantidadeDeAlunos++;
-        System.out.println("O aluno " + aluno.nome + " se matriculou");
+public class Main {
+
+    public static void main(String[] args) {
+
+        Faculdade faculdade = new Faculdade(
+                "Faculdade Tech", "Belo Horizonte", 1995, "Privada", 1200
+        );
+
+        Curso curso1 = new Curso("ADS", "ADS01", 4, 3000, "Noturno");
+        Curso curso2 = new Curso("Sistemas de Informação", "SI01", 4, 3200, "Noturno");
+        Curso curso3 = new Curso("Engenharia de Software", "ES01", 5, 3500, "Diurno");
+
+        faculdade.adicionarCurso(curso1);
+        faculdade.adicionarCurso(curso2);
+        faculdade.adicionarCurso(curso3);
+
+        Aluno aluno1 = new Aluno("João", "2023001", 20, "joao@email.com", curso1);
+        Aluno aluno2 = new Aluno("Maria", "2023002", 21, "maria@email.com", curso2);
+        Aluno aluno3 = new Aluno("Pedro", "2023003", 19, "pedro@email.com", curso3);
+
+        faculdade.adicionarAluno(aluno1);
+        faculdade.adicionarAluno(aluno2);
+        faculdade.adicionarAluno(aluno3);
+
+        faculdade.mostrarCursos();
+        faculdade.mostrarAlunos();
+
+        aluno1.estudar();
+        aluno2.estudar();
+        aluno3.estudar();
+    }
+}
+
+class Faculdade {
+
+    String nome;
+    String cidade;
+    int anoFundacao;
+    String tipo;
+    int quantidadeAlunos;
+
+    ArrayList<Aluno> alunos = new ArrayList<>();
+    ArrayList<Curso> cursos = new ArrayList<>();
+
+    public Faculdade(String nome, String cidade, int anoFundacao, String tipo, int quantidadeAlunos) {
+        this.nome = nome;
+        this.cidade = cidade;
+        this.anoFundacao = anoFundacao;
+        this.tipo = tipo;
+        this.quantidadeAlunos = quantidadeAlunos;
     }
 
-    void adicionaCurso(Curso curso) {
-        quantidadeDeCursos++;
-        System.out.println("O curso " + curso.nome + " foi adicionado");
+    public void adicionarAluno(Aluno aluno) {
+        alunos.add(aluno);
+        System.out.println("Aluno " + aluno.nome + " foi adicionado na faculdade.");
+    }
+
+    public void adicionarCurso(Curso curso) {
+        cursos.add(curso);
+        System.out.println("Curso " + curso.nome + " foi adicionado na faculdade.");
+    }
+
+    public void mostrarAlunos() {
+        System.out.println("\nLista de alunos:");
+        for (Aluno a : alunos) {
+            System.out.println(a.nome);
+        }
+    }
+
+    public void mostrarCursos() {
+        System.out.println("\nLista de cursos:");
+        for (Curso c : cursos) {
+            System.out.println(c.nome);
+        }
     }
 }
 
 class Aluno {
-    String nome;
-    String cpf;
-    String email;
-    String dataDeNascimento;
-    String cursoMatriculado;
 
-    void matriculaEmUmCurso(Curso curso) {
-        cursoMatriculado = curso.nome;
-        curso.adicionaAluno(this);
-        System.out.println("O aluno " + nome + " se matriculou no curso " + curso.nome);
+    String nome;
+    String matricula;
+    int idade;
+    String email;
+    Curso curso;
+
+    public Aluno(String nome, String matricula, int idade, String email, Curso curso) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.idade = idade;
+        this.email = email;
+        this.curso = curso;
+    }
+
+    public void estudar() {
+        System.out.println(nome + " está estudando o curso de " + curso.nome);
+    }
+
+    public void matricular() {
+        System.out.println(nome + " se matriculou no curso " + curso.nome);
     }
 }
 
 class Curso {
+
     String nome;
     String codigo;
-    String descricao;
-    int quantidadeDeAlunosMatriculados;
-    String coordenador;
+    int duracao;
+    double mensalidade;
+    String turno;
 
-    void adicionaAluno(Aluno aluno) {
-        quantidadeDeAlunosMatriculados++;
-        System.out.println("O aluno " + aluno.nome + " se matriculou no curso " + nome);
+    public Curso(String nome, String codigo, int duracao, double mensalidade, String turno) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.duracao = duracao;
+        this.mensalidade = mensalidade;
+        this.turno = turno;
     }
-}
 
-public class Main {
-    public static void main(String[] args) {
-        Faculdade Cotemig = new Faculdade();
-        cotemig.nome = "Faculdade Cotemig";
-        cotemig.endereco = "Belo Horizonte, MG";
-        cotemig.quantidadeDeAlunos = 0;
-        cotemig.quantidadeDeCursos = 0;
-        cotemig.reitor = "Moema Belo";
+    public void iniciarCurso() {
+        System.out.println("O curso " + nome + " foi iniciado.");
+    }
 
-        Aluno aluno1 = new Aluno();
-        aluno1.nome = "Gustavo";
-        aluno1.cpf = "123.456.789-10";
-        aluno1.email = "gustavo@aluno.faculdadecotemig.br";
-        aluno1.dataDeNascimento = "23/01/2005";
-
-        Aluno aluno2 = new Aluno();
-        aluno2.nome = "Pedro";
-        aluno2.cpf = "987.654.321-12";
-        aluno2.email = "pedro@aluno.faculdadecotemig.br";
-        aluno2.dataDeNascimento = "15/06/1995";
-
-        Aluno aluno3 = new Aluno();
-        aluno3.nome = "Maria";
-        aluno3.cpf = "456.789.012-34";
-        aluno3.email = "maria@aluno.faculdadecotemig.br";
-        aluno3.dataDeNascimento = "30/08/1997";
-
-        Curso curso1 = new Curso();
-        curso1.nome = "Publicidade e Propaganda";
-        curso1.codigo = "PP";
-        curso1.descricao = "Curso de Publicidade e Propaganda";
-        curso1.quantidadeDeAlunosMatriculados = 0;
-        curso1.coordenador = "Dr. Lucas Smith";
-
-        Curso curso2 = new Curso();
-        curso2.nome = "Engenharia de Computação";
-        curso2.codigo = "ECO";
-        curso2.descricao = "Curso de Engenharia de Computação";
-        curso2.quantidadeDeAlunosMatriculados = 0;
-        curso2.coordenador = "Dr. Maria";
-
-        Curso curso3 = new Curso();
-        curso3.nome = "Matemática";
-        curso3.codigo = "MAT";
-        curso3.descricao = "Curso de Matemática";
-        curso3.quantidadeDeAlunosMatriculados = 0;
-        curso3.coordenador = "Dr. José";
-
-        cotemig.adicionaAluno(aluno1);
-        cotemig.adicionaAluno(aluno2);
-        cotemig.adicionaAluno(aluno3);
-
-        cotemig.adicionaCurso(curso1);
-        cotemig.adicionaCurso(curso2);
-        cotemig.adicionaCurso(curso3);
-
-        aluno1.matriculaEmUmCurso(curso1);
-        aluno2.matriculaEmUmCurso(curso2);
-        aluno3.matriculaEmUmCurso(curso3);
-
-        System.out.println("Quantidade de alunos na Faculdade Cotemig: " + cotemig.quantidadeDeAlunos);
-        System.out.println("Quantidade de cursos na Faculdade Cotemig: " + cotemig.quantidadeDeCursos);
-        System.out.println("Quantidade de alunos no curso de Publicidade e Propaganda: " + curso1.quantidadeDeAlunosMatriculados);
-        System.out.println("Quantidade de alunos no curso de Engenharia de Computação: " + curso2.quantidadeDeAlunosMatriculados);
-        System.out.println("Quantidade de alunos no curso de Matemática: " + curso3.quantidadeDeAlunosMatriculados);
+    public void mostrarInformacoes() {
+        System.out.println("Curso: " + nome + " | Duração: " + duracao + " anos");
     }
 }
